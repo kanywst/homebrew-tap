e848e1977c2888435f1b8ec449dafba40200a9b0
kanywst <kanywst12@gmail.com>
2026-09-14 22:53:48 +0900

    wtfi3: update to v0.3.0

--- Formula/wtfi3.rb
+++ Formula/wtfi3.rb
@@ -1,8 +1,8 @@
 class Wtfi3 < Formula
   desc "Visualize who talks to whom on a WiFi network you administer"
   homepage "https://github.com/kanywst/wtfi3"
-  url "https://github.com/kanywst/wtfi3/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "cbf26c75a4579f13dc7c80e65ed239bce0396dc83f79480a304dbc45c562318c"
+  url "https://github.com/kanywst/wtfi3/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "ec55d7daf41bc9ad4559751cb1b61675b51fa30f1cd576803a31f80a2eeb14a7"
   license "MIT"
   head "https://github.com/kanywst/wtfi3.git", branch: "main"
 
